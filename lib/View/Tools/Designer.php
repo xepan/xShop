bc07d2b9507d84e08890b04921aeb54c50231662
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-12-23 14:42:08 +0530

    Ready for designer

--- lib/View/Tools/Designer.php
+++ lib/View/Tools/Designer.php
@@ -4,8 +4,10 @@ namespace xShop;
 
 class View_Tools_Designer extends \componentBase\View_Component{
 	public $html_attributes=array(); // ONLY Available in server side components
+	
 	function init(){
 		parent::init();
+		$this->add('View_Warning');
 	}
 
 	// defined in parent class
