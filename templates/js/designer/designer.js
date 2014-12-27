4e94661604b6379aea2bbbbb134c455d02e108f2
sinharakesh <rksinha.btech@gmail.com>
2014-12-27 19:07:13 +0530

    option array json encoded

--- templates/js/designer/designer.js
+++ templates/js/designer/designer.js
@@ -587,6 +587,7 @@ Image_Component = function (params){
 }
 
 Save_Component = function (params){
+	var self = this;
 	this.parent=undefined;
 	// this.text = params.text != undefined?params.text:'Enter Text';
 	this.init = function(designer,canvas){
@@ -600,9 +601,32 @@ Save_Component = function (params){
 
 	this.renderTool = function(parent){
 		this.parent = parent;
+		this.component_option_array = [];
 		tool_btn = $('<div class="btn btn-deault">Save</div>').appendTo(parent.find('.xshop-designer-tool-topbar-buttonset'));
 		tool_btn.click(function(event){
-			console.log($(this).text());
+			$(self.designer_tool.components).each(function(index,component){
+				$(self.designer_tool.components[index].options).each(function(index,options){
+					self.component_option_array.push(JSON.stringify(options));
+				});
+			});
+			self.component_option_array = [];
+
+			//Save all option 
+			// $.ajax({
+			// 		url: 'index.php?page=xShop_page_designer_save',
+			// 		type: 'GET',
+			// 		data: {param1: 'value1'},
+			// 	})
+			// 	.done(function(ret) {
+			// 		console.log("success");
+			// 	})
+			// 	.fail(function() {
+			// 		console.log("error");
+			// 	})
+			// 	.always(function() {
+			// 		console.log("complete");
+			// 	});	
+
 		});
 	}
 }
