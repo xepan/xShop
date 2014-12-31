6726a05e17edb6508f6ffb076b964bcf17f6df97
sinharakesh <rksinha.btech@gmail.com>
2014-12-31 13:36:17 +0530

    designer save button working

---
+++ page/designer/save.php
@@ -0,0 +1,8 @@
+<?php
+
+class page_xShop_page_designer_save extends Page {
+	function page_index(){
+		
+		
+	}
+}		
--- templates/js/designer/plugins/Save.js
+++ templates/js/designer/plugins/Save.js
@@ -12,33 +12,42 @@ Save_Component = function (params){
 	}
 
 	this.renderTool = function(parent){
+		var self = this;
+		this.page = undefined;
+		this.layout = undefined;
 		this.parent = parent;
-		this.component_option_array = [];
+		this.layout_array = [];
 		tool_btn = $('<div class="btn btn-deault btn-xs"><i class="glyphicon glyphicon-floppy-saved"></i><br>Save</div>').appendTo(parent.find('.xshop-designer-tool-topbar-buttonset'));
+		
 		tool_btn.click(function(event){
-			console.log(self.designer_tool);
-			$(self.designer_tool.components).each(function(index,component){
-				$(self.designer_tool.components[index].options).each(function(index,options){
-					self.component_option_array.push(JSON.stringify(options));
+			self.layout_array = {};
+			$.each(self.designer_tool.pages_and_layouts,function(index,pages){
+				self.page = index;
+				self.layout_array[index]= new Object;
+				$.each(self.designer_tool.pages_and_layouts[index],function(index,layout){
+					self.layout = index;
+					self.layout_array[self.page][index]=new Object;
+					self.layout_array[self.page][self.layout]['components']=[];
+					$.each(layout.components,function(index,component){
+						self.layout_array[self.page][self.layout]['components'].push(JSON.stringify(component.options));
+					});
 				});
 			});
-			self.component_option_array = [];
-
-			//Save all option 
-			// $.ajax({
-			// 		url: 'index.php?page=xShop_page_designer_save',
-			// 		type: 'GET',
-			// 		data: {param1: 'value1'},
-			// 	})
-			// 	.done(function(ret) {
-			// 		console.log("success");
-			// 	})
-			// 	.fail(function() {
-			// 		console.log("error");
-			// 	})
-			// 	.always(function() {
-			// 		console.log("complete");
-			// 	});	
+			console.log(self.layout_array);
+			$.ajax({
+					url: 'index.php?page=xShop_page_designer_save',
+					type: 'GET',
+					data: {param1: 'value1'},
+				})
+				.done(function(ret) {
+					console.log("success");
+				})
+				.fail(function() {
+					console.log("error");
+				})
+				.always(function() {
+					console.log("complete");
+				});	
 
 		});
 	}
