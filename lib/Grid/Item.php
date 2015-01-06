381717f0f599aa0cad2ef635e1985a542c5b81ad
Administrator <adam@www.deshvikas.com>
2015-01-06 16:03:13 +0530

    Grid Action View working on

--- lib/Grid/Item.php
+++ lib/Grid/Item.php
@@ -19,7 +19,27 @@ class Grid_Item extends \Grid{
 		$this->addColumn('expander','custom_fields');
 		$this->addColumn('expander','specifications');
 		$this->addColumn('expander','attachments');
+		$this->addColumn('actions','actions');
 		parent::recursiveRender();
+	}
+
+	function init_actions($field){
+	    $this->columns[$field]['tpl']=$this->add('GiTemplate')->loadTemplate('column/item-grid');
+
+	    $m=$this->model;
+
+	    $do_flag = $this->add('VirtualPage')->set(function($p)use($m){
+	        $name=$m->load($_GET['id'])['name'];
+
+	        // $m->flag();
+	        echo "sdf";
+	        return $p->js()->univ()->alert('You have flagged '.$name)->execute();
+	    });
+
+	    $this->on('click','.do-set-default')->univ()->ajaxec([$do_flag->getURL(), 'id'=>$this->js()->_selectorThis()->closest('tr')->data('id')]);
+	}
+	function format_actions($field){
+	    $this->current_row_html[$field] = $this->columns[$field]['tpl']->render();
 	}
 
 }
