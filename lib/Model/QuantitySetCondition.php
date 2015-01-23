92965c1d0a110a19284f49a6915058055bce2b4d
sinharakesh <rksinha.btech@gmail.com>
2015-01-23 14:01:31 +0530

    qty condition model added

---
+++ lib/Model/QuantitySetCondition.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace xShop;
+
+class Model_QuantitySetCondition extends \Model_Table{
+	public $table = 'xshop_item_quantity_set_conditions';
+
+	function init(){
+		parent::init();
+
+		$this->hasOne('xShop/QuantitySet','quantityset_id');
+		$this->hasOne('xShop/CategoryItemCustomFields','itemcustomfiledasso_id');
+		// $this->addField('name'); // To give special name to a quantity Set Conditions.. leave empty to have qty value here too
+		$this->addField('price')->type('money')->mandatory(true);
+
+		$this->add('dynamic_model/Controller_AutoCreator');
+	}
+}
