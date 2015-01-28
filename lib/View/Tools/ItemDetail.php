3a46c390bac02a68dbc6c6163a6411b4cb69d63c
sinharakesh <rksinha.btech@gmail.com>
2015-01-28 12:48:31 +0530

    item detail working

--- lib/View/Tools/ItemDetail.php
+++ lib/View/Tools/ItemDetail.php
@@ -4,16 +4,33 @@ namespace xShop;
 
 class View_Tools_ItemDetail extends \componentBase\View_Component{
 	public $html_attributes=array(); // ONLY Available in server side components
+	
 	function init(){
 		parent::init();
-		// $_GET['xsnb_item_id'];
-		// echo $_GET['xsnb_item_id'];
-		// exit;
+		
 		$this->addClass('xshop-item');
-		$this->js(true)->_load('jquery-elevatezoom');
+		// $this->js(true)->_load('jquery-elevatezoom');
 		$this->api->stickyGET('xsnb_item_id');
 		$config_model=$this->add('xShop/Model_Configuration');
 		$item=$this->add('xShop/Model_Item');
+		if(!$_GET['xsnb_item_id'])
+			return;
+
+		$item->load($_GET['xsnb_item_id']);		
+		$this->setModel($item);
+		
+		//Show Images
+		$col_width = "12";
+		if($this->html_attributes['show-image']){
+			$col_width = "4";
+			//$images = $this->add('xShop/View_Lister_itemImages',null,'item_images');
+			//$images->setModel($this->add('xShop/Model_ItemImages')->addCondition('item_id',$_GET['xsnb_item_id']));
+		}else{
+			$this->template->tryDel('item_images');
+		}
+		
+		//Item Offer Supplier Shipping Info
+		$this->add('LoremIpsum',null,'item_offer_supplier_shipping_info')->setLength(1,100);
 
 		if($item['show_attachment']){
 			$attachment_model=$this->add('xShop/Model_Attachments');
@@ -22,63 +39,20 @@ class View_Tools_ItemDetail extends \componentBase\View_Component{
 			$this->template->set('attachment_url',$attachment_model['attachment_url']);
 			$this->template->set('attachment_name',$attachment_model['name']);
 		}else
-			$this->template->tryDel('xshop_item_attachment');	
+			$this->template->tryDel('xshop_item_attachment');
 		
-		//Live Edit of item Detail (server site live edit )
-		if( $this->api->edit_mode == true ){		
-				$this->js(true)->_load('xshopContentUpdate');
-		}else{ 
-			$this->template->tryDel('xshop_item_detail_live_edit_start');
-			$this->template->tryDel('xshop_item_detail_live_edit_end');
-		}
-		 //end  
+		
 
-		//PANEL OPTIONS
-			$this->template->trySetHTML('xshop_item_detail_panel_label',$this->html_attributes['xshop_item_detail_panel_label']);
-			if(!$this->html_attributes['xshop_item_detail_panel']){
-				$this->template->tryDel('xshop_item_detail_panel_start');
-				$this->template->tryDel('xshop_item_detail_panel_end');
-			}
-
-		//END OF PANEL OPTIONS
-		if($_GET['xsnb_item_id']){
-			$item->load($_GET['xsnb_item_id']);		
-		}else{
-			return;
-		}
-
-		$this->add('View',null,'description123')->setHtml($item['description']);
+		// $this->add('View',null,'description123')->setHtml($item['description']);
 		//adding tag to item detail options
-		$this->template->Set('xshop_item_tags',str_replace(',', " ", $item['tags']));	
-		
-		$details = $this->add('xShop/View_Lister_CustomFields',null,'item_custom_fields');
-		$custom_field_model=$this->add('xShop/Model_ItemCustomFieldAssos');
-		$custom_field_model->addCondition('item_id',$_GET['xsnb_item_id']);
-		$details->setModel($custom_field_model);
-							
-		// do adding multiple images of a single item
-		// $images = $this->add('xShop/View_Lister_itemImages',null,'item_images');			
-		// $images->setModel($this->add('xShop/Model_ItemImages')->addCondition('item_id',$_GET['xsnb_item_id']));	
-		
-		$this->setModel($item);
+		$this->template->Set('xshop_item_tags',str_replace(',', " ", $item['tags']));
+
 
 		$this->api->template->trySet('page_title',$item['name']);
 
-		// if(!$item['allow_enquiry'])
-		// 	$this->template->tryDel('xshop_item_enquiry');	
-
-		// if(!$item['show_supplier_detail'])
-		// 	$this->template->tryDel('xshop_item_supplier');
-		// $this->template->trySet('supplier_caption',$this->html_attributes['xshop_pd_supplier_caption']?:'Supplier');
-
 		if(!$item['show_price']){
-			// throw new \Exception("Error Processing Request", 1);
 			$this->template->tryDel('xshop_itemdetail_price');
 		}
-
-		// if(!$item['show_manufacturer_detail'])
-		// 	$this->template->tryDel('xshop_item_manufacturer');
-		// $this->template->trySet('manufacturer_caption',$this->html_attributes['xshop_pd_manufacturer_caption']?:'Manufacturer');
 		
 		if($this->html_attributes['xshop_item_detail_images']==1){
 			$this->template->tryDel('xshop_item_detail_images');	
@@ -93,17 +67,7 @@ class View_Tools_ItemDetail extends \componentBase\View_Component{
 					$this->template->trySetHTML('xshop_item_discus',"<div class='alert alert-info'>Place Your Discus Code...in Configuration</div>");			
 			} 
 		}
-
-		// if($item['allow_saleable']){
-		// 	$this->template->trySetHTML('aj',str_replace('"', "'", $this->js(null, $this->js()->_selector('body')->attr('xshop_add_item_id',$this->model->id))->_selector(' .xshop-cart ')->trigger('reload')->_render()));			
-		// }else{
-		// 	$this->current_row_html['aj']='';
-		// 	$this->template->tryDel('xshop_item_cart_btn');	
-		// }	
 		
-		$this->template->trySetHTML('supplier_description_copy',$this->model['supplier_description']);
-		$this->template->trySetHTML('manufacturer_description_copy',$this->model['manufacturer_description']);
-
 		$this->add('View',null,'send_button')->set('Send Enquiry')->addClass('btn btn-default');
 		
 		//add custom btn in item detail
@@ -128,6 +92,32 @@ class View_Tools_ItemDetail extends \componentBase\View_Component{
 				//add AddToCart View
 				$this->add('xShop/View_Item_AddToCart',array('name'=>'cust_'.$this->model->id,'item_model'=>$this->model,'show_custom_fields'=>1,'show_price'=>$this->model['show_price'], 'show_qty_selection'=>1),'xshop_item_cart_btn');
 			}
+
+		// Item Detail and Comments specification
+		$tabs = $this->add('Tabs',null,'xshop_item_detail_information');
+		$detail_tab = $tabs->addTab('Detail');
+		$specification_tab = $tabs->addTab('Specification');
+		$enquiry_tab = $tabs->addTab('Enquiry');
+
+		$item_description = $item['description'];
+		//Live Edit of item Detail (server site live edit )
+		if( $this->api->edit_mode == true ){		
+			$this->js(true)->_load('xshopContentUpdate');
+			$str = '<div class="epan-container epan-sortable-component epan-component  ui-sortable component-outline epan-sortable-extra-padding ui-selected xshop-item-detail-content-live-edit" component_type="Container" id="xshop_item_detail_content_live_edit_"'.$item['id'].'>';
+			$str.= $item_description;
+			$str.="</div>";
+			
+			// $btn Todoooooooooooooooo???????????????/	
+			$btn = 'onclick="javascript:$(this).univ().producDetailUpdate(';
+			$btn.= '\'xshop_item_detail_content_live_edit_'.$item['id'].'\' , \''.$item['id'].'\' , \''.$item['sku'].'\')"';
+			$str.='<div id="xshop_item_detail_live_edit_update" class="btn btn-danger pull-right btn-block" '.$btn.'>Update</div>';
+			$item_description = $str;
+		}
+		//Detail tabs	
+		$detail_tab->add('View')->setHtml($item_description);
+		$detail_tab->add('View')->setHtml($item_description);
+		
+		//
 
 		$enquiry_form=$this->add('Form',null,'xshop_item_enquiry_form');
 		$enquiry_form->addField('line','name');
