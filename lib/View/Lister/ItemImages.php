70b820d6cc7094864ba7a40d0293c80ede8ff954
sinharakesh <rksinha.btech@gmail.com>
2015-01-29 20:34:03 +0530

    Item Images tool

--- lib/View/Lister/ItemImages.php
+++ lib/View/Lister/ItemImages.php
@@ -6,15 +6,23 @@ class View_Lister_ItemImages extends \CompleteLister{
 	function setModel($model){
 		parent::setModel($model);
 		
-		// cloneing the model and set first images  
+		// cloneing the model and set first images
 		$one_image = clone $model;
 		$one_image->tryLoadAny();
-		$this->template->trySet('zoom3_image_url',$one_image['image_url']?:"epan-components/xShop/templates/images/item_no_image.png ");
-			
+		$link ='index.php/?page=image&image='; 
+		$first_image = $link.'epan-components/xShop/templates/images/item_no_image.png';
+		if($one_image['item_image']){
+			$first_image = $link.$one_image['item_image'].'&width=200';
+			$first_large_image = $link.$one_image['item_image'].'&width=500';
+		}
+		
+		$this->template->trySet('zoom3_image_url',$first_image);
+		$this->template->trySet('zoom3_large_image_url',$first_image);
 	}
 	
 	function formatRow(){
-		$this->current_row['image_url'] = $this->model['image_url'];
+		$this->current_row['thumb_image_url'] = 'index.php/?page=image&image='.$this->model['item_image'].'&width=200&height=200';
+		$this->current_row['image_url'] = 'index.php/?page=image&image='.$this->model['item_image'];
 	}
 
 	function defaultTemplate(){
